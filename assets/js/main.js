a67a589e1583663b5b21a010623718f42dd181e0
safiull <safiullalam9931@gmail.com>
2021-09-25 08:21:10 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(".toggle-password").click(function() {
+
+        $(this).toggleClass("toggle-password");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+
+    $(".toggle-confirm-password").click(function() {
+
+        $(this).toggleClass("toggle-confirm-password");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+
+});
