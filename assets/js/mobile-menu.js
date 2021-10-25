413803ab7f1a5649607360d76e60c1c468a31728
safiull <safiullalam9931@gmail.com>
2021-10-25 16:27:31 +0600

    Two new pages added

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("mobile-menu").style.width = "100%";
+    document.getElementById("mobile-menu").style.right = "0";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("mobile-menu").style.width = "0%";
+    document.getElementById("mobile-menu").style.right = "-100%";
 }
